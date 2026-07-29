4f9016d6e247794283a5099f97db8febfc9c5fec
itskills30-master <it.skills.30@gmail.com>
2026-07-29 13:37:11 +0700

    Update jemaat-baru.js
    update api exec

--- js/jemaat-baru.js
+++ js/jemaat-baru.js
@@ -9,7 +9,7 @@ Version : 2.0
 API: AMBIL DARI Code.gs
 ====================================================*/
 const API_URL =
-"https://script.google.com/macros/s/AKfycbz9Ht4aLy8DTlyRRAF19xLQyQe8OT-9jcJjWU1-PebY8U3bCRo89PX-RtojeKBZBt6rSw/exec";
+"https://script.google.com/macros/s/AKfycbwPFk9QNoX-ixzyeYtKadR9_NQxsUXTB8j_2MVyqwWzOgxEqEGWEmDm_LftRb_yt0_Fdw/exec";
 
 
 /*====================================================
