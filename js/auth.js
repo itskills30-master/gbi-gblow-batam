8753a2f282e91541d289f97e91e6a7c735d42871
itskills30-master <it.skills.30@gmail.com>
2026-08-07 16:57:42 +0700

    Update auth.js
    versi

--- js/auth.js
+++ js/auth.js
@@ -31,7 +31,7 @@ Versi 1.1.0 = Update Besar (Major Update)
 Naikkan versi setiap ada perubahan
 yang mempengaruhi cache/session.
 ====================================================*/
-const APP_VERSION = "1.0.13";
+const APP_VERSION = "1.0.15";
 
 
 /*====================================================
